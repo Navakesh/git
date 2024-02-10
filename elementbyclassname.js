9681f1f24063e311477b06f07530bf1bc2f5d0dc
Navakesh <navakesh42@gmail.com>
2024-02-10 10:18:33 +0530

    elementbyclassname

---
+++ elementbyclassname.js
@@ -0,0 +1,6 @@
+var fruits = document.getElementsByClassName("fruit");
+  fruits[2].style.backgroundColor = "yellow";
+
+  for (var i = 0; i < fruits.length; i++) {
+    fruits[i].style.fontWeight = "bold";
+  }
